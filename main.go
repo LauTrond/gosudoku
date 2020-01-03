6559b5e82bfe8eb6e69514327ff2e45953473b08
liuchang <liuchang@yimian.com.cn>
2020-01-03 14:41:20 +0800

    recurse find multi results

--- main.go
+++ main.go
@@ -4,24 +4,7 @@ import (
 	"fmt"
 )
 
-var (
-	loop9 [9]bool
-	loop3 [3]bool
-)
-
-const puzzleGuest = `
- 1    5 4
- 96  7  3
-   2   1 
-      8 7
- 85 6   2
-  4      
- 3     9 
-  9 3   5
-   54  6 
-`
-
-const puzzleOrigin = `
+const puzzleExpert = `
  1    5 4
  96  7   
    2   1 
@@ -34,95 +17,153 @@ const puzzleOrigin = `
 `
 
 func main() {
-	s := NewSituation(puzzleOrigin)
-	s.show("start", -1, -1)
-	for {
-		found := false
-		for r := range loop9 {
-			for c := range loop9 {
-				ex := 0
-				nx := -1
-				for n := range loop9 {
-					if s.cellNumberEx[r][c][n] {
-						ex++
-					} else {
-						nx = n
+	s := NewSituation(puzzleExpert)
+	s.Show("start", -1, -1)
+	result := recurseEval(s)
+	if len(result) > 0 {
+		for i, answer := range result {
+			ShowCells(answer, fmt.Sprintf("result %d", i), -1, -1)
+		}
+	} else {
+		s.Show("failed", -1, -1)
+	}
+}
+
+func recurseEval(s *Situation) []*[9][9]int {
+	consistency := eval(s)
+	if !consistency {
+		return nil
+	}
+	completed := s.Completed()
+	if completed {
+		cells := s.cells
+		return []*[9][9]int{&cells}
+	}
+
+	result := make([]*[9][9]int, 0)
+	choices := s.GuessChoices()
+	if len(choices) == 0 {
+		return nil
+	}
+	try := choices[0]
+
+	for _, n := range try.Nums {
+		s2 := s.Copy()
+		s2.Set(try.Row, try.Col, n)
+		s2.Show("Guess", try.Row, try.Col)
+		subResult := recurseEval(s2)
+
+		/*
+		if len(subResult) == 0 {
+			//exclude inconsistency guess
+			fmt.Printf("inconsistency: exclude (%d,%d):%d\n",
+				try.Row, try.Col, n + 1)
+		} else {
+			//exclude known solve
+			for _, answer := range subResult {
+				for r := range loop9 {
+					for c := range loop9 {
+						if s.cells[r][c] < 0 {
+							fmt.Printf("known answer: exclude (%d,%d):%d\n",
+								r, c, answer[r][c] + 1)
+							s.Exclude(r, c, answer[r][c])
+						}
 					}
-				}
-				if ex == 9 {
-					panic(fmt.Errorf("unsolved at (%d,%d)",r,c))
-				}
-				if ex == 8 && s.cellIs[r][c] != nx {
-					found = true
-					s.Set(r, c, nx)
-					s.show("cellNumberEx", r, c)
 				}
 			}
 		}
+		*/
+
+		result = append(result, subResult...)
+	}
+
+	return result
+}
+
+func eval(s *Situation) bool {
+	for {
+		changed := false
 		for r := range loop9 {
 			for n := range loop9 {
-				ex := 0
-				cx := -1
+				ex := s.NewExcluding()
 				for c := range loop9 {
-					if s.rowNumberColumnEx[r][n][c] {
-						ex++
-					} else {
-						cx = c
-					}
+					ex.Test(r, c, n)
 				}
-				if ex == 8 && s.cellIs[r][cx] != n {
-					found = true
-					s.Set(r, cx, n)
-					s.show("rowNumberColumnEx", r, cx)
+				done, changed2, consistency, cell := ex.Apply()
+				if !consistency {
+					fmt.Printf("inconsistency locate %d in row %d\n", n+1, r)
+					return false
+				}
+				changed = changed || changed2
+				if done {
+					s.Show(fmt.Sprintf("locate %d in row %d", n+1, r),
+						cell.Row, cell.Col)
 				}
 			}
 		}
 		for c := range loop9 {
 			for n := range loop9 {
-				ex := 0
-				rx := -1
+				ex := s.NewExcluding()
 				for r := range loop9 {
-					if s.columnNumberRowEx[c][n][r] {
-						ex++
-					} else {
-						rx = r
-					}
+					ex.Test(r, c, n)
 				}
-				if ex == 8 && s.cellIs[rx][c] != n {
-					found = true
-					s.Set(rx, c, n)
-					s.show("columnNumberRowEx", rx, c)
+				done, changed2, consistency, cell := ex.Apply()
+				if !consistency {
+					fmt.Printf("inconsistency locate %d in column %d\n", n+1, c)
+					return false
+				}
+				changed = changed || changed2
+				if done {
+					s.Show(fmt.Sprintf("locate %d in column %d", n+1, c),
+						cell.Row, cell.Col)
 				}
 			}
 		}
 		for R := range loop3 {
 			for C := range loop3 {
 				for n := range loop9 {
-					ex := 0
-					rx := -1
-					cx := -1
-					for r := range loop3 {
-						for c := range loop3 {
-							if s.blockNumberCellEx[R][C][n][r][c] {
-								ex++
-							} else {
-								rx = r
-								cx = c
-							}
+					ex := s.NewExcluding()
+					for rr := range loop3 {
+						for cc := range loop3 {
+							r := R * 3 + rr
+							c := C * 3 + cc
+							ex.Test(r, c, n)
 						}
 					}
-					rr := R*3 + rx
-					cc := C*3 + cx
-					if ex == 8 && s.cellIs[rr][cc] != n {
-						found = true
-						s.Set(rr, cc, n)
-						s.show("blockNumberCellEx", rr, cc)
+					done, changed2, consistency, cell := ex.Apply()
+					if !consistency {
+						fmt.Printf("inconsistency locate %d in block (%d,%d)\n", n+1, R, C)
+						return false
+					}
+					changed = changed || changed2
+					if done {
+						s.Show(fmt.Sprintf("locate %d in block (%d,%d)", n+1, R, C),
+							cell.Row, cell.Col)
 					}
 				}
 			}
 		}
-		if !found {
+		for r := range loop9 {
+			for c := range loop9 {
+				ex := s.NewExcluding()
+				for n := range loop9 {
+					ex.Test(r, c, n)
+				}
+				done, changed2, consistent, cell := ex.Apply()
+				if !consistent {
+					fmt.Printf("inconsistency cell exclude (%d,%d)\n", r, c)
+					return false
+				}
+				changed = changed || changed2
+				if done {
+					s.Show("cell exclude", cell.Row, cell.Col)
+				}
+			}
+		}
+		if !changed {
 			break
 		}
 	}
+
+	return true
 }
--- situation.go
+++ situation.go
@@ -2,36 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
+var (
+	loop9 [9]bool
+	loop3 [3]bool
+)
+
 type Situation struct {
-	//rowNumberColumnEx[r][n][c] = true
-	//r行的n不可能存在于c列
-	rowNumberColumnEx [9][9][9]bool
+	//cellExclude[r][c][n] = true
+	//means cell(r,c) cannot be n
+	cellExclude [9][9][9]bool
 
-	//columnNumberRowEx[c][n][r] = true
-	//c列的n不可能存在于r行
-	columnNumberRowEx [9][9][9]bool
+	//cell[r][c] = n
+	//n >=0 : cell(r,c) is n
+	//n == -1 : cell(r,c) not decided
+	cells [9][9]int
 
-	//blockNumberCellEx[R][C][n][r][c] = true
-	//块(R,C)的n不可能存在于(r,c)
-	blockNumberCellEx [3][3][9][3][3]bool
-
-	//cellNumberEx[r][c][n] = true
-	//r行c列不可能是n
-	cellNumberEx [9][9][9]bool
-
-	//cellNumber[r][c] = n
-	//r行c列是n
-	cellIs [9][9]int
+	showCount int
 }
 
 func NewSituation(puzzle string) *Situation {
 	var s Situation
 	for r := range loop9 {
 		for c := range loop9 {
-			s.cellIs[r][c] = -1
+			s.cells[r][c] = -1
 		}
 	}
 	lines := strings.Split(strings.Trim(puzzle, "\n"), "\n")
@@ -51,54 +48,100 @@ func (s *Situation) Copy() *Situation {
 }
 
 //设置r行c列是n
-func (s *Situation) Set(r, c, n int) {
-	s.cellIs[r][c]=n
-	R := r/3
-	C := c/3
+func (s *Situation) Set(r, c, n int) bool {
+	if s.cells[r][c] == n {
+		return false
+	}
+	s.cells[r][c] = n
+	R := r / 3
+	C := c / 3
 
 	for n0 := range loop9 {
 		if n0 != n {
-			s.Exclude(r, c, n0)
+			s.cellExclude[r][c][n0] = true
+			//s.Exclude(r, c, n0)
 		}
 	}
 	for r0 := range loop9 {
 		if r0 != r {
-			s.Exclude(r0, c, n)
+			s.cellExclude[r0][c][n] = true
+			//s.Exclude(r0, c, n)
 		}
 	}
 	for c0 := range loop9 {
 		if c0 != c {
-			s.Exclude(r, c0, n)
+			s.cellExclude[r][c0][n] = true
+			//s.Exclude(r, c0, n)
 		}
 	}
 
 	for r0 := range loop3 {
 		for c0 := range loop3 {
-			rr := R * 3 + r0
-			cc := C * 3 + c0
+			rr := R*3 + r0
+			cc := C*3 + c0
 			if rr != r || cc != c {
-				s.Exclude(rr, cc, n)
+				s.cellExclude[rr][cc][n] = true
+				//s.Exclude(rr, cc, n)
 			}
 		}
 	}
+	return true
 }
 
-func (s *Situation) Exclude(r, c, n int) {
-	R := r/3
-	C := c/3
-
-	s.cellNumberEx[r][c][n] = true
-	s.rowNumberColumnEx[r][n][c] = true
-	s.columnNumberRowEx[c][n][r] = true
-	s.blockNumberCellEx[R][C][n][r-R*3][c-C*3] = true
+func (s *Situation) Exclude(r, c, n int) bool {
+	if s.cellExclude[r][c][n] {
+		return false
+	}
+	s.cellExclude[r][c][n] = true
+	return true
 }
 
-func (s *Situation) show(title string, r, c int) {
+type GuessItem struct {
+	RowCol
+	Nums []int
+}
+
+func (s *Situation) GuessChoices() []*GuessItem {
+	m := map[RowCol][]int{}
+	for r := range loop9 {
+		for c := range loop9 {
+			if s.cells[r][c] >= 0 {
+				continue
+			}
+
+			cell := RowCol{r, c}
+			for n := range loop9 {
+				if !s.cellExclude[r][c][n] {
+					m[cell] = append(m[cell], n)
+				}
+			}
+		}
+	}
+
+	result := make([]*GuessItem, 0)
+	for rc, nums := range m {
+		result = append(result, &GuessItem{
+			RowCol: rc,
+			Nums:   nums,
+		})
+	}
+	sort.Slice(result, func(i, j int) bool {
+		return len(result[i].Nums) < len(result[j].Nums)
+	})
+	return result
+}
+
+func (s *Situation) Show(title string, r, c int) {
+	ShowCells(&s.cells, fmt.Sprintf("%d %s", s.showCount, title), r, c)
+	s.showCount++
+}
+
+func ShowCells(cells *[9][9]int, title string, r, c int) {
 	fmt.Println("====================================")
 	fmt.Println(title)
 	for r1 := range loop9 {
 		for c1 := range loop9 {
-			if n1 := s.cellIs[r1][c1]; n1 >= 0 {
+			if n1 := cells[r1][c1]; n1 >= 0 {
 				if r1 == r && c1 == c {
 					fmt.Printf("[%d] ", n1+1)
 				} else {
@@ -115,5 +158,144 @@ func (s *Situation) show(title string, r, c int) {
 		if r1 == 2 || r1 == 5 {
 			fmt.Println("------------------------------------")
 		}
+	}
+}
+
+func (s *Situation) Completed() bool {
+	for r := range loop9 {
+		for c := range loop9 {
+			if s.cells[r][c] < 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+type Excluding struct {
+	s            *Situation
+	matchedCells map[RowCol]bool
+	matchedCount int
+	selectedCell RowCol
+	selectedN    int
+	sameBlock    bool
+	sameRow      bool
+	sameCol      bool
+	sameNum      bool
+}
+
+func (s *Situation) NewExcluding() *Excluding {
+	return &Excluding{
+		s:            s,
+		matchedCells: map[RowCol]bool{},
+		matchedCount: 0,
+		selectedCell: RowCol{-1, -1},
+		selectedN:    -1,
+		sameBlock:    true,
+		sameRow:      true,
+		sameCol:      true,
+		sameNum:      true,
+	}
+}
+
+func (ex *Excluding) Test(r, c, n int) {
+	cell := RowCol{r, c}
+	if !ex.s.cellExclude[cell.Row][cell.Col][n] {
+		if ex.matchedCount > 0 {
+			if ex.selectedCell.Row != cell.Row {
+				ex.sameRow = false
+			}
+			if ex.selectedCell.Col != cell.Col {
+				ex.sameCol = false
+			}
+			if ex.selectedCell.Block() != cell.Block() {
+				ex.sameBlock = false
+			}
+			if ex.selectedN != n {
+				ex.sameNum = false
+			}
+		}
+		ex.selectedCell = cell
+		ex.selectedN = n
+		ex.matchedCells[cell] = true
+		ex.matchedCount++
+	}
+}
+
+func (ex *Excluding) Apply() (done, changed, consistent bool, applyCell RowCol) {
+	switch ex.matchedCount {
+	case 0:
+		return false, false, false, ex.selectedCell
+	case 1:
+		r, c, n := ex.selectedCell.Row, ex.selectedCell.Col, ex.selectedN
+		if ex.s.cells[r][c] < 0 {
+			ex.s.Set(r, c, n)
+			return true, true, true, ex.selectedCell
+		}
+		if ex.s.cells[r][c] != n {
+			return false, false, false, ex.selectedCell
+		}
+		return false, false, true, ex.selectedCell //没有改变
+	default:
+		if !ex.sameNum {
+			return false, false, true, ex.selectedCell //没有改变
+		}
+
+		changed := false
+
+		collectionExclude := func(rc RowCol, n int) {
+			changed1 := false
+			if !ex.matchedCells[rc] {
+				changed1 = changed1 || ex.s.Exclude(rc.Row, rc.Col, n)
+			}
+			if changed1 {
+				fmt.Printf("collectionExclude(%d,%d,%d)\n", rc.Row, rc.Col, n + 1)
+			}
+			changed = changed || changed1
+		}
+
+		if ex.sameRow {
+			for c := range loop9 {
+				collectionExclude(RowCol{ex.selectedCell.Row, c}, ex.selectedN)
+			}
+		}
+		if ex.sameCol {
+			for r := range loop9 {
+				collectionExclude(RowCol{r, ex.selectedCell.Col}, ex.selectedN)
+			}
+		}
+		if ex.sameBlock {
+			for r := range loop3 {
+				for c := range loop3 {
+					collectionExclude(ex.selectedCell.Block().LeftTop().Add(r, c), ex.selectedN)
+				}
+			}
+		}
+		return false, changed, true, RowCol{-1, -1}
+	}
+}
+
+type RowCol struct {
+	Row, Col int
+}
+
+func (rc RowCol) Block() RowCol {
+	return RowCol{
+		rc.Row / 3,
+		rc.Col / 3,
+	}
+}
+
+func (rc RowCol) LeftTop() RowCol {
+	return RowCol{
+		rc.Row * 3,
+		rc.Col * 3,
+	}
+}
+
+func (rc RowCol) Add(r, c int) RowCol {
+	return RowCol{
+		rc.Row + r,
+		rc.Col + c,
 	}
 }
