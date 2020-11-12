c1a645d88671cb3ce5281855e7e02f53d6778a49
liuchang <liuchang@yimian.com.cn>
2020-11-12 16:00:18 +0800

    一堆优化

--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"sort"
 )
 
-const puzzleExpert = `
- 1    5 4
- 96  7   
-   2   1 
-      8 7
- 85 6   2
-  4      
- 3     9 
-  9 3   5
-   54  6 
-`
+var (
+	flagShowOnlyResult = flag.Bool("result-only", false, "")
+)
 
 func main() {
-	s := NewSituation(puzzleExpert)
-	s.Show("start", -1, -1)
+	flag.Parse()
+	puzzle := loadPuzzle()
+	s := NewSituation(puzzle)
+
+	if !*flagShowOnlyResult {
+		s.Show("start", -1, -1)
+	}
 	result := recurseEval(s)
 	if len(result) > 0 {
+		fmt.Printf("\n找到了 %d 个答案\n", len(result))
 		for i, answer := range result {
 			ShowCells(answer, fmt.Sprintf("result %d", i), -1, -1)
 		}
@@ -29,9 +32,30 @@ func main() {
 	}
 }
 
+func loadPuzzle() string {
+	input := io.Reader(os.Stdin)
+	if flag.Arg(0) != "" {
+		f, err := os.Open(flag.Arg(0))
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		input = f
+	}
+
+	raw, err := ioutil.ReadAll(io.LimitReader(input, 1024))
+	if err != nil {
+		panic(err)
+	}
+
+	return string(raw)
+}
+
+// recurseEval 开始推断局势 s，并返回所有可能的终局。
+// 如果返回nil，表示这个局势有矛盾，不存在正确的解答。
 func recurseEval(s *Situation) []*[9][9]int {
-	consistency := eval(s)
-	if !consistency {
+	if !eval(s) {
 		return nil
 	}
 	completed := s.Completed()
@@ -40,17 +64,28 @@ func recurseEval(s *Situation) []*[9][9]int {
 		return []*[9][9]int{&cells}
 	}
 
+	//当前没有找到确定的填充选项，所以获取所有可能选项，然后猜一个。
 	result := make([]*[9][9]int, 0)
-	choices := s.GuessChoices()
+	choices := s.Choices()
 	if len(choices) == 0 {
 		return nil
 	}
+	sort.Slice(choices, func(i, j int) bool {
+		return len(choices[i].Nums) < len(choices[j].Nums)
+	})
 	try := choices[0]
+
+    tryNumsForShow := make([]int, len(try.Nums))
+    for i, n := range try.Nums {
+		tryNumsForShow[i] = n + 1
+	}
 
 	for _, n := range try.Nums {
 		s2 := s.Copy()
 		s2.Set(try.Row, try.Col, n)
-		s2.Show("Guess", try.Row, try.Col)
+		if !*flagShowOnlyResult {
+			s2.Show(fmt.Sprintf("在可能的选项里猜一个（全部选项：%v）", tryNumsForShow), try.Row, try.Col)
+		}
 		subResult := recurseEval(s2)
 		result = append(result, subResult...)
 	}
@@ -58,6 +93,9 @@ func recurseEval(s *Situation) []*[9][9]int {
 	return result
 }
 
+// eval 开始推断局势 s，直到没有找到确定的填充选项，不确保全部完成。
+// 通过 s.Completed() 可以判断是否已经填入所有数字。
+// 如果返回false，表示这个局势有矛盾。
 func eval(s *Situation) bool {
 	for {
 		changed := false
@@ -69,13 +107,17 @@ func eval(s *Situation) bool {
 				}
 				done, changed2, consistency, cell := ex.Apply()
 				if !consistency {
-					fmt.Printf("inconsistency locate %d in row %d\n", n+1, r)
+					if !*flagShowOnlyResult {
+						fmt.Printf("发生矛盾：第 %d 行没有单元格可以填入 %d\n", r+1, n+1)
+					}
 					return false
 				}
 				changed = changed || changed2
 				if done {
-					s.Show(fmt.Sprintf("locate %d in row %d", n+1, r),
-						cell.Row, cell.Col)
+					if !*flagShowOnlyResult {
+						s.Show(fmt.Sprintf("该行唯一可以填入 %d 的单元格", n+1),
+							cell.Row, cell.Col)
+					}
 				}
 			}
 		}
@@ -87,13 +129,17 @@ func eval(s *Situation) bool {
 				}
 				done, changed2, consistency, cell := ex.Apply()
 				if !consistency {
-					fmt.Printf("inconsistency locate %d in column %d\n", n+1, c)
+					if !*flagShowOnlyResult {
+						fmt.Printf("发生矛盾：第 %d 列没有单元格可以填入 %d\n", c+1, n+1)
+					}
 					return false
 				}
 				changed = changed || changed2
 				if done {
-					s.Show(fmt.Sprintf("locate %d in column %d", n+1, c),
-						cell.Row, cell.Col)
+					if !*flagShowOnlyResult {
+						s.Show(fmt.Sprintf("该列唯一可以填入 %d 的单元格", n+1),
+							cell.Row, cell.Col)
+					}
 				}
 			}
 		}
@@ -110,13 +156,17 @@ func eval(s *Situation) bool {
 					}
 					done, changed2, consistency, cell := ex.Apply()
 					if !consistency {
-						fmt.Printf("inconsistency locate %d in block (%d,%d)\n", n+1, R, C)
+						if !*flagShowOnlyResult {
+							fmt.Printf("发生矛盾：区块内没有单元格可以填入 %d\n", n+1)
+						}
 						return false
 					}
 					changed = changed || changed2
 					if done {
-						s.Show(fmt.Sprintf("locate %d in block (%d,%d)", n+1, R, C),
-							cell.Row, cell.Col)
+						if !*flagShowOnlyResult {
+							s.Show(fmt.Sprintf("区块内唯一可以填入 %d 的单元格", n+1),
+								cell.Row, cell.Col)
+						}
 					}
 				}
 			}
@@ -129,12 +179,16 @@ func eval(s *Situation) bool {
 				}
 				done, changed2, consistent, cell := ex.Apply()
 				if !consistent {
-					fmt.Printf("inconsistency cell exclude (%d,%d)\n", r, c)
+					if !*flagShowOnlyResult {
+						fmt.Printf("发生矛盾：%d 行 %d 列单元格没有可以填入的数字\n", r+1, c+1)
+					}
 					return false
 				}
 				changed = changed || changed2
 				if done {
-					s.Show("cell exclude", cell.Row, cell.Col)
+					if !*flagShowOnlyResult {
+						s.Show("这个单元格唯一可以填的数", cell.Row, cell.Col)
+					}
 				}
 			}
 		}
--- situation.go
+++ situation.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"strings"
 )
 
@@ -11,19 +10,23 @@ var (
 	loop3 [3]bool
 )
 
+// Situation 代表9*9数独的一个局势
+// 所有行、列、单元格值都是0~8，显示的时候加1处理。
 type Situation struct {
-	//cellExclude[r][c][n] = true
-	//means cell(r,c) cannot be n
+	//cellExclude[r][c][n] = true 表示单元格(r,c)不可能是n
 	cellExclude [9][9][9]bool
 
 	//cell[r][c] = n
-	//n >=0 : cell(r,c) is n
-	//n == -1 : cell(r,c) not decided
+	//n >=0 : 单元格(r,c) 填入了数字n
+	//n == -1 : 单元格(r,c) 还没填入数字
 	cells [9][9]int
 
 	showCount int
 }
 
+// 初始化一个数独谜题
+// puzzle 是一个9行的字符串（前后空行会自动去除），以空格和数字代表单元格
+// 其中空格代表未填入的单元格
 func NewSituation(puzzle string) *Situation {
 	var s Situation
 	for r := range loop9 {
@@ -33,7 +36,13 @@ func NewSituation(puzzle string) *Situation {
 	}
 	lines := strings.Split(strings.Trim(puzzle, "\n"), "\n")
 	for r, line := range lines {
+		if r > len(s.cells) {
+			panic(fmt.Errorf("row exceed"))
+		}
 		for c, n := range line {
+			if c > len(s.cells[r]) {
+				panic(fmt.Errorf("column exceed"))
+			}
 			if n >= '1' && n <= '9' {
 				s.Set(r, c, int(n-'1'))
 			}
@@ -47,10 +56,11 @@ func (s *Situation) Copy() *Situation {
 	return &s2
 }
 
-//设置r行c列是n
+//r 行 c 列填入 n
+//同时会执行同一互斥组的排除（修改 s.cellExclude）
 func (s *Situation) Set(r, c, n int) bool {
-	if s.cells[r][c] == n {
-		return false
+	if s.cells[r][c] != -1 {
+		panic(fmt.Errorf("reseting cell(%d,%d)=%d", r, c, n))
 	}
 	s.cells[r][c] = n
 	R := r / 3
@@ -59,19 +69,16 @@ func (s *Situation) Set(r, c, n int) bool {
 	for n0 := range loop9 {
 		if n0 != n {
 			s.cellExclude[r][c][n0] = true
-			//s.Exclude(r, c, n0)
 		}
 	}
 	for r0 := range loop9 {
 		if r0 != r {
 			s.cellExclude[r0][c][n] = true
-			//s.Exclude(r0, c, n)
 		}
 	}
 	for c0 := range loop9 {
 		if c0 != c {
 			s.cellExclude[r][c0][n] = true
-			//s.Exclude(r, c0, n)
 		}
 	}
 
@@ -81,14 +88,13 @@ func (s *Situation) Set(r, c, n int) bool {
 			cc := C*3 + c0
 			if rr != r || cc != c {
 				s.cellExclude[rr][cc][n] = true
-				//s.Exclude(rr, cc, n)
 			}
 		}
 	}
 	return true
 }
 
-func (s *Situation) Exclude(r, c, n int) bool {
+func (s *Situation) exclude(r, c, n int) bool {
 	if s.cellExclude[r][c][n] {
 		return false
 	}
@@ -101,7 +107,8 @@ type GuessItem struct {
 	Nums []int
 }
 
-func (s *Situation) GuessChoices() []*GuessItem {
+// 获取当前无法排除的所有填充选项。
+func (s *Situation) Choices() []*GuessItem {
 	m := map[RowCol][]int{}
 	for r := range loop9 {
 		for c := range loop9 {
@@ -125,14 +132,11 @@ func (s *Situation) GuessChoices() []*GuessItem {
 			Nums:   nums,
 		})
 	}
-	sort.Slice(result, func(i, j int) bool {
-		return len(result[i].Nums) < len(result[j].Nums)
-	})
 	return result
 }
 
 func (s *Situation) Show(title string, r, c int) {
-	ShowCells(&s.cells, fmt.Sprintf("%d %s", s.showCount, title), r, c)
+	ShowCells(&s.cells, fmt.Sprintf("<%d> %s", s.showCount, title), r, c)
 	s.showCount++
 }
 
@@ -184,6 +188,10 @@ type Excluding struct {
 	sameNum      bool
 }
 
+// 从当前局势 s 创建一个"排除"实例 Excluding
+// Excluding 用于找到可能的填充单元格选项
+// 给定一系列（9个）选项， 每个选项调用一次 Test(r,c,n)
+// 最后调用 Apply，如果 9 个选项有且只有 1 个合理，则填入到 s 中。
 func (s *Situation) NewExcluding() *Excluding {
 	return &Excluding{
 		s:            s,
@@ -221,37 +229,50 @@ func (ex *Excluding) Test(r, c, n int) {
 		ex.matchedCount++
 	}
 }
-
+// Apply 返回三种可能：
+// - A、所有选项都不合理，发生了矛盾。
+// - B、唯一合理的选项，则填入到局势中。
+// - C、唯一合理的选项，但单元格早前已经被填入该数字。
+// - D、唯一合理的选项，但单元格早前已经被填入不同的数字，发生了矛盾。
+// - E、超过一个可能选项，不能确定。
+// 返回值：
+// - done = true: 局势 s 已经填入数字。只有选项 B 会返回 done=true，
+// - changed = true：局势 s 发生了改变。和 done 不同的是，即使没有填入数字也可能发生了改变，因为排除了某些选项。
+// - consistent = false：发生了矛盾，如果之前的所有推断没有错，这个局势显然无法完成。
+// - applyCell：如果有唯一合理的选项，返回该单元格的位置。
 func (ex *Excluding) Apply() (done, changed, consistent bool, applyCell RowCol) {
 	switch ex.matchedCount {
 	case 0:
+		//A
 		return false, false, false, ex.selectedCell
 	case 1:
 		r, c, n := ex.selectedCell.Row, ex.selectedCell.Col, ex.selectedN
-		if ex.s.cells[r][c] < 0 {
+		switch ex.s.cells[r][c] {
+		case -1:
+			//B
 			ex.s.Set(r, c, n)
 			return true, true, true, ex.selectedCell
-		}
-		if ex.s.cells[r][c] != n {
+		case n:
+			//C
+			return false, false, true, ex.selectedCell //没有改变
+		default:
+			//D
 			return false, false, false, ex.selectedCell
 		}
-		return false, false, true, ex.selectedCell //没有改变
 	default:
 		if !ex.sameNum {
 			return false, false, true, ex.selectedCell //没有改变
 		}
 
-		changed := false
-
+		//开始执行"复杂排除"。参见文件最后的解析。
+		exChanged := false
 		complexExclude := func(rc RowCol, n int) {
-			changed1 := false
 			if !ex.matchedCells[rc] {
-				changed1 = changed1 || ex.s.Exclude(rc.Row, rc.Col, n)
+				if excluded := &ex.s.cellExclude[rc.Row][rc.Col][n]; !*excluded {
+					*excluded = true
+					exChanged = true
+				}
 			}
-			if changed1 {
-				fmt.Printf("complexExclude(%d,%d):%d\n", rc.Row, rc.Col, n + 1)
-			}
-			changed = changed || changed1
 		}
 
 		if ex.sameRow {
@@ -271,7 +292,8 @@ func (ex *Excluding) Apply() (done, changed, consistent bool, applyCell RowCol) 
 				}
 			}
 		}
-		return false, changed, true, RowCol{-1, -1}
+		//E
+		return false, exChanged, true, RowCol{-1, -1}
 	}
 }
 
@@ -299,3 +321,33 @@ func (rc RowCol) Add(r, c int) RowCol {
 		rc.Col + c,
 	}
 }
+
+/*
+
+=== 互斥组 ===
+
+同一行、或同一列、或同一区块。
+
+=== 复杂排除法 ===
+
+虽然有多个可能选项，但这些选项是同一个数N，而且都在相同的互斥组中。
+那么同一互斥组的其他单元格可以排除。
+例如局势：
+
+     1      |            | 5   #   4
+     9   6  |         7  | #   #   #
+            | 2       *  |     1
+------------------------------------
+            |            | 8       7
+     8   5  |     6      |         2
+         4  |            |
+------------------------------------
+     3      |            |     9
+         9  |     3      |         5
+            | 5   4      |     6
+
+留意右上区块，根据其他行列排除法，标记"#"的单元格都不能是 6，所以 6 必定在同一区块剩余的两个单元格中。
+刚好这两个选项同在另外一个互斥体（第 3 行），所以第 3 行其他单元格都不能是 6。
+标记 "*" 的单元格因此排除6，你没有其他简单的方法可以作出这个断言。
+
+*/
