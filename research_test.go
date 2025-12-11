7e89524a31d3c2283089af0f6efcca996ddfcc05
LiuChang <chang.liu@omc.com>
2025-12-11 22:28:59 +0800

    分支测试，未开发完成

---
+++ research_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestBranch(t *testing.T) {
+	puzzle, err := os.ReadFile("puzzles/hard-02.txt")
+	check(err)
+	*flagComplexGen = 0
+	s, tgr := ParseSituation(string(puzzle))
+	logicalEval(s, tgr)
+	s.Show("初始", -1, -1)
+
+	ctx := newSudokuContext()
+	count := ctx.recurseEval(DuplicateSituation(s), NewTrigger(), fmt.Sprintf("<%d>", s.Count()))
+	if count != 1 {
+		t.Fatalf("非唯一解：%d", count)
+	}
+	ShowCells(ctx.solutions[0], "解", -1, -1)
+}
+
+func logicalEval(s *Situation, t *Trigger) bool {
+	for {
+		rcn, ok := t.GetConfirm()
+		if !ok {
+			break
+		}
+		if s.Set(t, rcn) {
+			if len(t.Conflicts) > 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
